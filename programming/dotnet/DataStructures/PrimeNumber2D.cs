bae1282425a9104d4a3e09bb3c3dab783ed188ee
sumitchourasia <st.sumit01@gmail.com>
2020-01-03 19:48:20 +0530

    primeNumber2D program and primeNUmberAndAnagram2D program commited

---
+++ programming/dotnet/DataStructures/PrimeNumber2D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class PrimeNumber2D
+    {
+        public void PrimeNumber2DMethod()
+        {
+            int[][] array = new int[10][];
+            for(int i=0;i<10;i++)
+            {
+                array[i] = new int[100];
+            }
+
+            int number = 1;
+            int m = 0;
+            
+            for(int i=0;i<10;i++)
+            {
+                //take m and n to insert the prime number continuously.
+                //to increase the efficiency.
+                int n = 0;
+                for (int j=0;j<100;j++)
+                {
+                    if(Utility.Util.IsPrime(number))
+                    {
+                        array[m][n] = number;
+                        n++;
+                    }
+                    number++;
+                }
+                m++;
+            }
+
+            ////to print the prime numbers in the 2d array except 0.
+            Utility.Util.PrintPrimeNumberIn2D(array);
+
+        }
+
+
+      
+
+    }
+}
---
+++ programming/dotnet/DataStructures/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main( )
+        {
+            Console.WriteLine("enter your choice : ");
+            Console.WriteLine(" 8 -> PrimeNumbersIn2D program ");
+            Console.WriteLine(" 9 -> primeNumberAnagram2D program ");
+            Console.WriteLine(" 3 -> ");
+
+            int k = Utility.Util.ReadInt();
+
+            switch(k)
+            {
+                    case 1:
+                        
+                        
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 8:
+                        PrimeNumber2D prime = new PrimeNumber2D();
+                        prime.PrimeNumber2DMethod();
+                        break;
+
+                    case 9:
+                    PrimeNumberAnagram2D PrimeAnagram = new PrimeNumberAnagram2D();
+                    PrimeAnagram.PrimeNumberAnagramMethod();
+                    break;
+
+                default :
+                    Console.WriteLine("default run ");
+                    break;
+            }
+        }
+    }
+}
