8605e52a77d2585b5232d5ba88120ee486e3a4b3
sumitchourasia <st.sumit01@gmail.com>
2020-01-08 18:59:29 +0530

    updates

---
+++ programming/dotnet/Practice/MyGenericsClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Practice
+{
+    class MyGenericsClass<T>
+    {
+        StreamReader SR = new StreamReader(@"C: \Users\Bridgelabz\Desktop\bridgelabz\programming\dotnet\Practice\generictext.txt");
+
+
+
+        string[] array = new string[10];
+        
+        for()
+            {
+        SR.ReadLine();
+            }
+
+
+    }
+    class dothings
+}
