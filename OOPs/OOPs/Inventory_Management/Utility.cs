2b7e66af779b686ef989a7eff09e2eec3ac9a440
sumit <st.sumit01@gmail.com>
2020-01-14 16:19:55 +0530

    [sumit] add . adding remove and update method in inventory management program implementation

--- OOPs/OOPs/Inventory_Management/Utility.cs
+++ OOPs/OOPs/Inventory_Management/Utility.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
-using OOPs.Inventory_Details;
 
 namespace OOPs.Inventory_Management
 {
@@ -143,6 +142,10 @@ namespace OOPs.Inventory_Management
             }
         }
 
+        /// <summary>
+        /// Adds the rice.
+        /// </summary>
+        /// <param name="inventoryItemsObject">The inventory items object.</param>
         public static void AddRice(InventoryItems inventoryItemsObject)
         {
             List<ItemsData> rices = inventoryItemsObject.Rice;
@@ -155,44 +158,144 @@ namespace OOPs.Inventory_Management
             newRice.Price = ReadDouble();
             rices.Add(newRice);
             inventoryItemsObject.Rice = rices;
-        }  
+        }
 
+        /// <summary>
+        /// Adds the wheat.
+        /// </summary>
+        /// <param name="inventoryItemsObject">The inventory items object.</param>
         public static void AddWheat(InventoryItems inventoryItemsObject)
         {
             List<ItemsData> wheat = inventoryItemsObject.Wheat;
             ItemsData newWheat = new ItemsData();
+            Console.Write("enter the name :");
             newWheat.Name = ReadString();
+            Console.Write("enter the weight :");
             newWheat.Weight = ReadDouble();
+            Console.Write("enter the price :");
             newWheat.Price = ReadDouble();
+            ////adding new wheat item to the wheat list
             wheat.Add(newWheat);
+            ////adding the updated wheat list 
             inventoryItemsObject.Wheat = wheat;
         }
 
+        /// <summary>
+        /// Adds the pulses.
+        /// </summary>
+        /// <param name="inventoryItemsObject">The inventory items object.</param>
         public static void AddPulses(InventoryItems inventoryItemsObject)
         {
             List<ItemsData> pulses = inventoryItemsObject.Pulses;
             ItemsData newPulses = new ItemsData();
+            Console.Write("enter the name :");
             newPulses.Name = ReadString();
+            Console.Write("enter the weight :");
             newPulses.Weight = ReadDouble();
+            Console.Write("enter the price");
             newPulses.Price = ReadDouble();
             pulses.Add(newPulses);
             inventoryItemsObject.Pulses = pulses;
         }
 
+        /// <summary>
+        /// Removes the rice.
+        /// </summary>
+        /// <param name="inventoryItemsObject">The inventory items object.</param>
         public static void RemoveRice(InventoryItems inventoryItemsObject)
         {
-            List<ItemsData> rice = inventoryItemsObject.Rice;
-            ItemsData deleterice = new ItemsData();
-            Console.Write("Enter the name of the item to delete : ");
-            string name = ReadString();
-            foreach(var items in rice)
+            List<ItemsData> rices = inventoryItemsObject.Rice;
+            Console.Write("Enter the name of the rice item to delete : ");
+            string removeRice = ReadString();
+            foreach(var item in rices)
             {
-                if (items.Name == name)
-                    rice.Remove(items);
+                Console.WriteLine("inside foreach loop");
+                if (item.Name == removeRice)
+                {
+                    rices.Remove(item);
+                    break;
+                }
             }
-            inventoryItemsObject.Rice = rice;
+            inventoryItemsObject.Rice = rices;
         }
 
+        /// <summary>
+        /// Removes the specified inventory items object.
+        /// </summary>
+        /// <param name="inventoryItemsObject">The inventory items object.</param>
+        public static void Remove(InventoryItems inventoryItemsObject)
+        {
+            List<ItemsData>[] listOfItems = new List<ItemsData>[3];
+            listOfItems[0] = inventoryItemsObject.Rice;
+            listOfItems[1] = inventoryItemsObject.Wheat;
+            listOfItems[2] = inventoryItemsObject.Pulses;
+
+            Console.Write("enter the name of item to remove : ");
+            string removeNameItem = ReadString();
+            foreach(var items in listOfItems)
+            {
+                foreach(var item in items)
+                {
+                    if(item.Name == removeNameItem)
+                    {
+                        items.Remove(item);
+                        return;
+                    }
+                }
+            }
         }
+
+        /// <summary>
+        /// Updates the specified inventory items object.
+        /// </summary>
+        /// <param name="inventoryItemsObject">The inventory items object.</param>
+        public static void Update(InventoryItems inventoryItemsObject)
+        {
+            Console.WriteLine("inside update method");
+            List<ItemsData>[] listOfItems = new List<ItemsData>[3];
+            listOfItems[0] = inventoryItemsObject.Rice;
+            listOfItems[1] = inventoryItemsObject.Wheat;
+            listOfItems[2] = inventoryItemsObject.Pulses;
+
+            Console.Write("enter the name of item to update : ");
+
+            string removeNameItem = ReadString();
+            foreach (var items in listOfItems)
+            {
+                foreach (var item in items)
+                {
+                    if (item.Name == removeNameItem)
+                    {
+                        Console.WriteLine("enter the property to update : ");
+                        string property = ReadString();
+                       
+                        if(property == "Name")
+                        {
+                            Console.Write("enter new Name");
+                            string newName = ReadString();
+                            item.Name = newName;
+                            return;
+                        }
+                        else if(property == "Weight")
+                        {
+                            Console.Write("enter new Weight");
+                            double newWeight = ReadDouble();
+                            item.Weight = newWeight;
+                            return;
+                        }
+                        else if(property == "Price")
+                        {
+                            Console.Write("enter new price");
+                            double newPrice = ReadDouble();
+                            item.Price = newPrice;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        
     }
 }
