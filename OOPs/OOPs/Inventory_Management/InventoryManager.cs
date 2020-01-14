a585f429a940f2398408936a5cbd0946bf70ec33
sumit <st.sumit01@gmail.com>
2020-01-14 13:20:12 +0530

    [sumit] update the switch case to call AddRice() AddWheat() AddPulse() DeleteRice() UpdateRice()

--- OOPs/OOPs/Inventory_Management/InventoryManager.cs
+++ OOPs/OOPs/Inventory_Management/InventoryManager.cs
@@ -9,71 +9,81 @@ namespace OOPs.Inventory_Management
         /// <summary>
         /// Managers this instance.
         /// </summary>
-        public void InventoryManagerMethod()
+        public void InventoryManagerMethod(InventoryItems inventoryItemsObject)
         {
-                    do
-                    {
-                        Console.WriteLine("1.  adding  rice");
-                        Console.WriteLine("2.  adding wheat");
-                        Console.WriteLine("3.  adding pulses");
-                        Console.WriteLine("4.  deleting rice");
-                        Console.WriteLine("5.  deleting wheat");
-                        Console.WriteLine("6.  deleting pulses");
-                        Console.WriteLine("7.  Updating rice");
-                        Console.WriteLine("8.  Updating wheat");
-                        Console.WriteLine("9.  Updating pulses");
-                        Console.WriteLine("10. viewing details");
+            int choice = 0;
+            string proceed = " ";
+            do
+            {
+                Console.WriteLine("enter your choice");
+                Console.WriteLine("1.  adding  rice");
+                Console.WriteLine("2.  adding wheat");
+                Console.WriteLine("3.  adding pulses");
+                Console.WriteLine("4.  deleting rice");
+                Console.WriteLine("5.  deleting wheat");
+                Console.WriteLine("6.  deleting pulses");
+                Console.WriteLine("7.  Updating rice");
+                Console.WriteLine("8.  Updating wheat");
+                Console.WriteLine("9.  Updating pulses");
+                Console.WriteLine("10. viewing details");
+                Console.WriteLine(" -1. exit");
 
-                        int choice = Convert.ToInt32(Console.ReadLine());
-                        switch (choice)
-                        {
-                            case 1:
-                                Utility.AddRice();
-                                break;
+                choice = Utility.ReadInt();
 
-                            case 2:
-                                 Utility.AddWheat();
-                                break;
+                switch (choice)
+                {
+                    case 1:
+                        Utility.AddRice(inventoryItemsObject);
+                        Utility.PrintInventoryItem(inventoryItemsObject);
+                        break;
 
-                            case 3:
-                                 Utility.AddPulses();
-                                break;
+                    case 2:
+                            Utility.AddWheat(inventoryItemsObject);
+                        break;
 
-                            case 4:
-                                 Utility.DeleteRice();
-                                break;
+                    case 3:
+                            Utility.AddPulses(inventoryItemsObject);
+                        break;
 
-                            case 5:
-                                 Utility.DeleteWheat();
-                                break;
+                    case 4:
+                            Utility.RemoveRice(inventoryItemsObject);
+                            Utility.PrintInventoryItem(inventoryItemsObject);
+                            break;
+                /*
+                    case 5:
+                            Utility.RemoveWheat();
+                        break;
 
-                            case 6:
-                                 Utility.DeletePulse();
-                                break;
+                    case 6:
+                            Utility.DeletePulse();
+                        break;
+                */
+                    case 7:
+                            Utility.UpdateRice();
+                        break;
 
-                            case 7:
-                                 Utility.Update();
-                                break;
+                    case 8:
+                            Utility.UpdateWheat();
+                        break;
 
-                            case 8:
-                                 Utility.UpdateWheat();
-                                break;
+                    case 9:
+                        Utility.UpdatePulses();
+                        break;
 
-                            case 9:
-                                Utility.UpdatePulses();
-                                break;
+                    case 10:
+                        Utility.ViewDetails();
+                        break;
+                    
+                    case -1:
+                        return;
 
-                            case 10:
-                                Utility.ViewDetails();
-                                break;
-
-                            default:
-                                Console.WriteLine("Enter the proper number");
-                                break;
-                        }
-                    }
-                    while (character != 'n');
-
+                    default:
+                        Console.WriteLine("Enter the proper number");
+                        break;
+                }
+                Console.Write("do you eant to continue  yes/no:");
+                proceed = Utility.ReadString();
+                }while (proceed != "no");                   
         }
     }
 }
