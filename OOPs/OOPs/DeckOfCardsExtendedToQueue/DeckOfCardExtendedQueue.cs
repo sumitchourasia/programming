0f1fde6797490568201763f3828a285985efee1a
sumit <st.sumit01@gmail.com>
2020-01-17 10:02:00 +0530

    [sumit] add . adding instruction to call the sort method

--- OOPs/OOPs/DeckOfCardsExtendedToQueue/DeckOfCardExtendedQueue.cs
+++ OOPs/OOPs/DeckOfCardsExtendedToQueue/DeckOfCardExtendedQueue.cs
@@ -2,6 +2,9 @@
 
 namespace OOPs.DeckOfCardsExtendedToQueue
 {
+    /// <summary>
+    /// program implementation of Deck of card using Queue datastructure implemented using linked list
+    /// </summary>
     public class DeckOfCardExtendedQueue
     {
         /// <summary>
@@ -14,18 +17,29 @@ namespace OOPs.DeckOfCardsExtendedToQueue
             int[] Ranks = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
 
             string[,] DeckOfCards = new string[4, 13];
-
+            
+            ////initialise the array to store all the cards 
             Utility.InitializeArray(DeckOfCards, Suits, Ranks);
+            
+            ////shuffle the deck of cardd
             Utility.Shuffle(DeckOfCards);
 
+            ////create the Queue to hold all the players
             QueuePlayer queuePlayer = Utility.CreateQueuePlayer();
 
+            ////add four player into the queue
             Utility.AddPlayersInQueue(queuePlayer);
 
+            ////ditribute the cards in the DecksOfCard array to all the players in the queue.
             Utility.DistributeCards(queuePlayer,DeckOfCards);
 
-            Utility.PrintQueuePlayer(queuePlayer);
+            ////sort the cards of each off the player in the queue
+            Utility.Sort(queuePlayer);
 
+            Console.WriteLine("after sorting : ");
+
+            ////print the entire queue.
+            Utility.PrintQueuePlayer(queuePlayer);
         }
     }
 }
