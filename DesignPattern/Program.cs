abc49dd46c0a34fff2e8470d2ce6acef1cd579ae
sumit <st.sumit01@gmail.com>
2020-01-22 17:25:58 +0530

    [sumit] Add . adding cases for Prototype and Adaptor design patterns

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -5,29 +5,40 @@ namespace DesignPattern
     /// <summary>
     /// class that contains the main method
     /// </summary>
-    class Program
+    public class Program
     {
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
-        static void Main(string[] args)
+        public static void Main( )
         {
             Console.WriteLine("Enter your choice");
             Console.WriteLine("1 -> Singleton design pattern ");
             Console.WriteLine("2 -> FactoryDesignPattern ");
+            Console.WriteLine("3 -> Prototype Design Pattern ");
             //// take the input from the user
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch(choice)
             {
                 case 1:
+                   
+                    break;
+                case 2:
                     ////to execute the factoryDesignPattern program.
                     FactoryDesignPattern.MobileFactoryTest factoryDesignObject = new FactoryDesignPattern.MobileFactoryTest();
                     factoryDesignObject.MobileFactoryTestMethod();
                     break;
-                case 2:
-
+                case 3:
+                    ////to execute the Prototype Design Pattern program
+                    PrototypeDesignPattern.PrototypeDesignPattern prototypeObject = new PrototypeDesignPattern.PrototypeDesignPattern();
+                    prototypeObject.PrototypeDesignPatternMethod();
+                    break;
+                case 4:
+                    ////to execute the Adaptor Design Pattern program
+                    AdaptorDesignPattern.AdaptorDesignPatternTest adaptorDesignObject = new AdaptorDesignPattern.AdaptorDesignPatternTest();
+                    adaptorDesignObject.AdaptorDesignPatternTestMethod();
                     break;
             }
         }
