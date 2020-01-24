f1b9c97fae4da9dd254d923c4e6801eb1fcedc42
sumit <st.sumit01@gmail.com>
2020-01-24 16:45:25 +0530

    [sumit]Add . adding singleton design pattern program implementation

---
+++ DesignPattern/SingletonDesignPattern/LazyInitializedSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DesignPattern.SingletonDesignPattern
+{
+    /// <summary>
+    /// class for LazyInitializedSingleton pattern
+    /// </summary>
+    public class LazyInitializedSingleton
+    {
+        private string name ;
+        private static LazyInitializedSingleton obj;
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get => this.name; set => this.name = value; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LazyInitializedSingleton"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        private LazyInitializedSingleton(string name)
+        {
+            this.name = name;
+        }
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public static LazyInitializedSingleton GetInstance(string name)
+        {
+            if(obj == null)
+               obj = new LazyInitializedSingleton(name);
+            else
+                Console.WriteLine("Object not created");
+            return obj;
+        }
+    }
+}
---
+++ DesignPattern/SingletonDesignPattern/SingleDesignPatternTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DesignPattern.SingletonDesignPattern
+{
+    /// <summary>
+    /// class for singleton desing pattern
+    /// </summary>
+    public class SingleDesignPatternTest
+    {
+        /// <summary>
+        /// operation for SingleDesignPatternTestMethod
+        /// </summary>
+        public void SingleDesignPatternTestMethod()
+        {
+            Console.WriteLine("Singleton Design Pattern");
+            GetThreeInstance();
+        }
+
+        /// <summary>
+        /// Gets the three instance.
+        /// </summary>
+        private static void GetThreeInstance()
+        {
+            string name = string.Empty;
+            LazyInitializedSingleton obj;
+            for(int i =0;i<3;i++)
+            {
+                Console.WriteLine("enter the name for the object : ");
+                 name = Console.ReadLine();
+                obj = LazyInitializedSingleton.GetInstance(name);
+                if (obj != null)
+                    Console.WriteLine("name : {0}",obj.Name);
+                else
+                    Console.WriteLine("object not created");
+            }
+        }
+    }
+}
