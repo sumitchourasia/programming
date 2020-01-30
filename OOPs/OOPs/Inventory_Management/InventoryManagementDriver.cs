ecf9c01397647ae0eadef24ca91c9108f221de6d
sumit <st.sumit01@gmail.com>
2020-01-30 13:10:39 +0530

    [sumit]Refactor. modifying the documentation and format in InventoryManagementDriver class

--- OOPs/OOPs/Inventory_Management/InventoryManagementDriver.cs
+++ OOPs/OOPs/Inventory_Management/InventoryManagementDriver.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace OOPs.Inventory_Management
 {
+    /// <summary>
+    /// invntory management driver class
+    /// </summary>
     public class InventoryManagementDriver
     {
+        /// <summary>
+        /// Inventory management driver method.
+        /// </summary>
         public void InventoryManagementDriverMethod()
         {
             try
             {
                 string path = @"C:\Users\Bridgelabz\Documents\GitHub\programming\OOP's\OOPs\OOPs\Inventory_Management\InventoryItemsFile.json";
-
                 string StringOfJson = Utility.ReadFile(path);
-
-             //   Console.WriteLine(StringOfJson + "string of json");
-
+                // Console.WriteLine(StringOfJson + "string of json");
                 InventoryItems fileList = Utility.DeserializeTheObject(StringOfJson);
-
                 Utility.PrintInventoryItem(fileList);
-
                 InventoryManager inventoryManager = new InventoryManager();
                 inventoryManager.InventoryManagerMethod(fileList);
-
             }
             catch (Exception e)
             {
