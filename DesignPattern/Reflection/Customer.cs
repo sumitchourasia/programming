c8b12bf8a359126b7521408ba9775d20a68b8731
sumit <st.sumit01@gmail.com>
2020-02-04 14:25:29 +0530

    [sumit]Add. adding Reflection Design Pattern program

---
+++ DesignPattern/Reflection/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Reflection
+{
+    public class Customer
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Customer"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="name">The name.</param>
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Customer"/> class.
+        /// </summary>
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+        /// <summary>
+        /// Prints the identifier.
+        /// </summary>
+        public void PrintId()
+        {
+            Console.WriteLine("Id is:{0}", this.Id);
+        }
+        /// <summary>
+        /// Prints the name.
+        /// </summary>
+        public void PrintName()
+        {
+            Console.WriteLine("Name is:{0}", this.Name);
+        }
+    }
+}
---
+++ DesignPattern/Reflection/TestReflection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace DesignPattern.Reflection
+{
+    public class TestReflection
+    {
+        /// <summary>
+        /// Tests this instance.
+        /// </summary>
+        public void Test()
+        {
+            ////Type is class where iam creating the object and calling the inbuilt function GetType.
+            Type type = Type.GetType("DesignPattern.Reflection.Customer");
+
+            ////printing the full name.
+            Console.WriteLine("Full Name :{0}", type.FullName);
+
+            ////printing the Class name.
+            Console.WriteLine("Class name is:{0}", type.Name);
+            Console.WriteLine("Methods in customer class ");
+
+            ////storing the number of methods in an array which is of MethodInfo class.
+            MethodInfo[] methods = type.GetMethods();
+
+            ////Traversing the data present in the method object.
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+            Console.WriteLine("Properties in Customer Class");
+
+            ////PropertyInfo is a class where iam storing the properties of customer class in the form of array.
+            PropertyInfo[] properties = type.GetProperties();
+
+            ////Traversing the data present in the property object.
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+            Console.WriteLine("Constructors in customer class");
+
+            ////ConstructorInfo is a class where iam storing the constructors of customer class in the form of array.
+            ConstructorInfo[] constructors = type.GetConstructors();
+
+            ////Traversing the data present in the constructor object.
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+
+    }
+}
